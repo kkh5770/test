836d55fa094f6889def4d01a9b470caa5f9886b4
kkh5770 <kkh5770@hotmail.com>
2020-03-11 11:08:01 +0900

    membervo 수정

--- src/domain/MemberVo.java
+++ src/domain/MemberVo.java
@@ -1,5 +1,20 @@
 package domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
 public class MemberVo {
+	private String id;
+	private String password;
+	private String name;
+	private String addr;
 
 }
