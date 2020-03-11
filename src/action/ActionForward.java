522a7715281cb16a73e09c9f55f56336d0a0eccd
kkh5770 <kkh5770@hotmail.com>
2020-03-11 11:02:51 +0900

    ActionForward 수정

--- src/action/ActionForward.java
+++ src/action/ActionForward.java
@@ -1,5 +1,14 @@
 package action;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class ActionForward {
 	private String path;
 	private boolean redirect;
